5cc81b73072a09f72306642a4161e27ff963c2b7
Jose Alves <jose.sousalves@gmail.com>
2022-05-30 21:41:30 +0100

    feat(app): imports fonts css files to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { GlobalStyle } from '@styles';
 import theme from '@styles/theme/theme';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
+import '../src/styles/fonts';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
