f8a546eeec6d6361a4a19f9b2c758fd8a6eba5cb
Jose Alves <jose.sousalves@gmail.com>
2022-05-30 21:43:56 +0100

    feat(theme): adds theme files

---
+++ src/styles/theme/theme.ts
@@ -0,0 +1,10 @@
+const theme = {
+  fonts: {
+    archivoBlack: "'Archivo Black', sans-serif;",
+    archivo: "'Archivo', sans-serif;",
+  },
+  colors: {},
+  dropShadow: '',
+};
+
+export default theme;
