f750189cf6a2bdde3d5c49b81243bcfe0bb89920
Jose Alves <jose.sousalves@gmail.com>
2022-05-30 21:38:27 +0100

    feat(app): applies theme provider + globalstyles to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { GlobalStyle } from '@styles';
+import theme from '@styles/theme/theme';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
