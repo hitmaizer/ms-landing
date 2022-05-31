f1657e8272ff8b772dc1d28cd14db7885b7e734e
Jose Alves <jose.sousalves@gmail.com>
2022-05-31 11:56:43 +0100

    feat(theme): adds grayscale values to index theme file

--- src/styles/theme/index.ts
+++ src/styles/theme/index.ts
@@ -5,7 +5,19 @@ const theme = {
   breakpoints,
   mediaQueries,
 
-  colors: {},
+  colors: {
+    white: '#FFFFFF',
+    gray50: '#FAFAFA',
+    gray100: '#F4F4F5',
+    gray200: '#E4E4E7',
+    gray300: '#D4D4D8',
+    gray400: '#A1A1AA',
+    gray500: '#71717A',
+    gray600: '#52525B',
+    gray700: '#3F3F46',
+    gray800: '#27272A',
+    gray900: '#18181B',
+  },
 
   // default fontSizes
   fontSizes: [13, 15, 18, 22, 26, 32, 44, 64],
