59f03c6b8a1b9a18ce6da4f22a7e60821bdeeb8f
Jose Alves <jose.sousalves@gmail.com>
2022-05-31 11:55:44 +0100

    feat(theme): adds gray scale values to theme file

--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -3,7 +3,19 @@ const theme = {
     archivoBlack: "'Archivo Black', sans-serif;",
     archivo: "'Archivo', sans-serif;",
   },
-  colors: {},
+  colors: {
+    white: '#FFFFFF',
+    gray50: '#FAFAFA',
+    gray100: '#F4F4F5',
+    gray200: '#E4E4E7',
+    gray300: '#D4D4D8',
+    gray400: '#A1A1AA',
+    gray500: '#71717A',
+    gray600: '#52525B',
+    gray700: '#3F3F46',
+    gray800: '#27272A',
+    gray900: '#18181B',
+  },
   dropShadow: '',
 };
 
