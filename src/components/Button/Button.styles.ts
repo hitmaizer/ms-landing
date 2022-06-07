b4e8fdd5dee9c8cf9a6916bc17915dc3ad288231
Jose Alves <jose.sousalves@gmail.com>
2022-06-07 20:46:40 +0100

    style(Button): updates Button component style rules

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -2,22 +2,22 @@ import styled, { css } from 'styled-components';
 import { ButtonProps } from './Button.types';
 
 export const Button = styled.button<ButtonProps>`
-  font-family: ${({ theme }) => theme.fonts.archivoBlack};
-  padding: 16px 24px;
-  font-weight: 900;
-  font-size: 16px;
+  font-family: ${({ theme }) => theme.fonts.text};
+  font-weight: 700;
+  font-size: 18px;
   line-height: 17px;
   transition: all 300ms ease;
-
+  background-color: transparent;
+  color: ${({ theme }) => theme.colors.mutedGray};
   ${({ outline }) =>
     outline &&
     css`
-      color: ${({ theme }) => theme.colors.white};
-      border: 1px solid ${({ theme }) => theme.colors.gray50};
+      color: ${({ theme }) => theme.colors.mutedGray};
+      border: 1px solid ${({ theme }) => theme.colors.mutedGray};
       background-color: transparent;
       &:hover {
         background-color: ${({ theme }) => theme.colors.gray50};
-        color: ${({ theme }) => theme.colors.gray900};
+        color: ${({ theme }) => theme.colors.offWhite};
       }
-    `}
+    `};
 `;
