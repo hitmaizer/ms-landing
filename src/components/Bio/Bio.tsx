946a6c9550e3bf54e4074cb3f52daf33868c0857
Jose Alves <jose.sousalves@gmail.com>
2022-06-07 18:59:48 +0100

    refactor(Bio): removes line breaks from Bio text

--- src/components/Bio/Bio.tsx
+++ src/components/Bio/Bio.tsx
@@ -8,14 +8,13 @@ const Bio = ({ children, ...rest }: BioProps) => {
   return (
     <S.Bio {...rest}>
       <Block>
-        <Text fontWeight="bold" color="#fff">
+        <Text fontWeight="regular">
           From the traditional jazz quartet with the guitar as a harmonic
-          instrument, QMS looks for their own groundbreaking new sound. <br />
-          The main focus in this project is still giving free pass to the
-          musicians to express themselves over the themes they&apos;re playing.{' '}
-          <br />
-          The quartet was brought together under the musical empathy they
-          achieve in their regular jams.
+          instrument, QMS looks for their own groundbreaking new sound. The main
+          focus in this project is still giving free pass to the musicians to
+          express themselves over the themes they&apos;re playing. The quartet
+          was brought together under the musical empathy they achieve in their
+          regular jams.
         </Text>
         {children}
       </Block>
