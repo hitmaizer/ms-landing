8eca75e4e37aa5e878d1a1ecc84dc9773c516bdc
Jose Alves <jose.sousalves@gmail.com>
2022-06-07 15:22:06 +0100

    feat(Footer): adds Footer component files

---
+++ src/components/Footer/Footer.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { flexbox, layout, space } from 'styled-system';
+
+export const Footer = styled.div`
+  ${flexbox}
+  ${layout}
+  ${space}
+  width: 100%;
+  height: 20vh;
+`;
