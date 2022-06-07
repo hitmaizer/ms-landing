6f40f2795974161b9e3a4481517183c860ca57c1
Jose Alves <jose.sousalves@gmail.com>
2022-06-07 21:56:40 +0100

    feat(react-i18next): adds translation strings for en and pt

---
+++ src/intl/intl.ts
@@ -0,0 +1,11 @@
+export const translationsPt = {
+  bio: 'Este quarteto surge em 2009, a par do seu primeiro projecto com edição discográfica (QMS “Bloco de Notas” - Encomenda) e pretende da mesma forma interpretar e dar a conhecer temas do seu bloco de notas. Do quarteto tradicional de Jazz com a guitarra como instrumento harmónico Mário Santos procura uma sonoridade própria e inovadora. O grande objectivo continua a ser o de criar momentos em que os músicos possuem toda a liberdade de se expressar sobre os temas que interpretam. Este quarteto nasce da cumplicidade sonora encontrada na regularidade dos seus encontros musicais.',
+  store: 'Loja',
+  contact: 'Contacto',
+};
+
+export const translationsEn = {
+  bio: 'From the traditional jazz quartet with the guitar as a harmonic instrument, QMS looks for their own groundbreaking new sound. The main focus in this project is still giving free pass to the musicians to express themselves over the themes theyre playing. The quartet was brought together under the musical empathy they achieve in their regular jams.',
+  store: 'Store',
+  contact: 'Get in touch',
+};
