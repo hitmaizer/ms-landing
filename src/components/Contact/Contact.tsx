fc48b99b4f0a67eadfce2079de6949169015fe4e
Jose Alves <jose.sousalves@gmail.com>
2022-06-07 21:58:55 +0100

    refactor(intl): updates components to read strings from intl

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -3,14 +3,16 @@ import Block from '../Block';
 import Text from '../Text';
 import * as S from './Contact.styles';
 import { ContactProps } from './Contact.types';
+import { useTranslation } from 'react-i18next';
 
 const Contact = ({ children, ...rest }: ContactProps) => {
+  const { t } = useTranslation();
   return (
     <S.Contact {...rest}>
       <a href="mailto:maritosan@gmail.com">
         <Block display="flex" alignItems="center" justifyContent="center">
           <Text textTransform="uppercase" fontWeight="600">
-            Get in touch
+            {t('contact')}
           </Text>
           {children}
         </Block>
