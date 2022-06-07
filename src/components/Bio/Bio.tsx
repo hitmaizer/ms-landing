b583f8bbd927f1552d58f9dd89fbc1deee5a8658
Jose Alves <jose.sousalves@gmail.com>
2022-06-07 15:20:47 +0100

    refactor(Bio): updates markup of Bio paragraph

--- src/components/Bio/Bio.tsx
+++ src/components/Bio/Bio.tsx
@@ -10,11 +10,12 @@ const Bio = ({ children, ...rest }: BioProps) => {
       <Block>
         <Text fontWeight="bold" color="#fff">
           From the traditional jazz quartet with the guitar as a harmonic
-          instrument, QMS looks for their own groundbreaking new sound. The main
-          focus in this project is still giving free pass to the musicians to
-          express themselves over the themes they&apos;re playing. The quartet
-          was brought together under the musical empathy they achieve in their
-          regular jams.
+          instrument, QMS looks for their own groundbreaking new sound. <br />
+          The main focus in this project is still giving free pass to the
+          musicians to express themselves over the themes they&apos;re playing.{' '}
+          <br />
+          The quartet was brought together under the musical empathy they
+          achieve in their regular jams.
         </Text>
         {children}
       </Block>
