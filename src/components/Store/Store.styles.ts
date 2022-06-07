863d6fa32fe07367d3886aa084052792cd10363f
Jose Alves <jose.sousalves@gmail.com>
2022-06-07 15:25:03 +0100

    feat(Store): adds Store component files

---
+++ src/components/Store/Store.styles.ts
@@ -0,0 +1,25 @@
+import { RightArrowAlt } from '@styled-icons/boxicons-regular';
+import styled from 'styled-components';
+import { flexbox, gridGap, layout, space } from 'styled-system';
+
+export const Arrow = styled(RightArrowAlt)`
+  width: 24px;
+  height: 24px;
+  color: ${({ theme }) => theme.colors.mutedGray};
+  transition: color 300ms ease;
+`;
+
+export const Store = styled.div`
+  ${flexbox}
+  ${space}
+  ${layout}
+  ${gridGap}
+  width: 25vw;
+  cursor: pointer;
+  &:hover {
+    p,
+    ${Arrow} {
+      color: ${({ theme }) => theme.colors.offWhite};
+    }
+  }
+`;
