4e162238a8897598cc948bbf5d5a67df38b6b4f1
Jose Alves <jose.sousalves@gmail.com>
2022-06-07 21:58:07 +0100

    feat(Button): updates Button prop types

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -3,4 +3,7 @@ import { ReactNode } from 'react';
 export interface ButtonProps {
   children?: ReactNode;
   outline?: boolean;
+  onClick: () => unknown;
+  ident?: string | undefined;
+  currLang?: string | undefined;
 }
